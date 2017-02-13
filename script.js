231bd95ac7ec482907842e1bc961b12b941727ec
Kevin Jiang <kevinjiang@Kevins-MacBook-Pro.local>
2017-02-13 18:58:38 -0500

    Added link pages

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
+
+// Animate Text on Frontpage
 $(function () {
-    $('.tlt').textillate();
-})
+  $('.tlt').textillate({
+    loop: true,
+    in: {
+      effect: 'tada',
+      shuffle: true,
+      delay: 100,
+    },
+    out: {
+      effect: 'tada',
+      delay: 100,
+      shuffle: true
+
+    }
+  });
+
+});
+
+// jQuery Hack to get footer to bottom
+$(function(){
+    $(window).resize(function(){
+        placeFooter();
+    });
+    placeFooter();
+    // hide it before it's positioned
+    $('#footer').css('display','inline');
+});
+
+function placeFooter() {
+    var windHeight = $(window).height();
+    var footerHeight = $('#footer').height();
+    var offset = parseInt(windHeight) - parseInt(footerHeight);
+    $('#footer').css('top',offset);
+}
